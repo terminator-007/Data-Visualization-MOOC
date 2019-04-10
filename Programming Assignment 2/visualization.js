4ee2a081de12b01ed6c2b8ae6f5ddb433df9371b
Varshini Prakash <icona.pop0@gmail.com>
2019-04-10 09:32:17 +0530

    Create visualization.js

---
+++ Programming Assignment 2/visualization.js
@@ -0,0 +1,49 @@
+var lesmis = echarts.init(document.getElementById('data'));
+data.nodes.forEach(function (node) {
+  node.name = node.label;
+  node.draggable = true;
+  node.value = 0;
+
+});
+
+data.links.forEach(function (link) {
+  data.nodes[link.source].value += link.value;
+  data.nodes[link.target].value += link.value;
+  data.nodes[link.source].symbolSize = data.nodes[link.source].value;
+  data.nodes[link.target].symbolSize = data.nodes[link.target].value;
+});
+
+data.nodes.forEach(function (node) {
+  node.symbolSize = 5 + node.value * 20 / 160; 
+})
+
+var option = {
+  title: {
+    text: 'Les Miserables',
+    subtext: 'Default layout',
+    top: 'bottom',
+    left: 'right'
+  },
+  animation: false,
+  tooltip: {},
+  series: [
+    {
+      name: 'Coappearance network in Les Miserables',
+      type: 'graph',
+      layout: 'force',
+      data: data.nodes,
+      links: data.links,
+      roam: true,
+      label: {
+        normal: {
+          position: 'right'
+        }
+      },
+      force: {
+        repulsion: 100
+      }
+    }
+  ]
+};
+
+lesmis.setOption(option);
